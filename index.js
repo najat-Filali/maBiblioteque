3d2923e849b51ab1652b762f038fe2e8f0f3c064
Najat Filali <filali_najat@hotmail.com>
2019-11-28 12:58:16 +0100

    feu qui marche !

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 function createBook(titre, prix, disp){
+    let available = document.querySelector('button'); 
 
-    let available = document.querySelector('[type="checkbox"]'); 
-
-    if (available.checked){
-        document.querySelector('#bibl').innerHTML +=`
-        <fieldset border="1px" background-color="green" >
-        <div class="livre">
-            <h2>titre du livre : <br> ${titre.toUpperCase()}</h2>
-            <h2>prix du livre : <br> ${prix}</h2>
-            <div> ${disp} </div>
-        </fieldset>
-        
-        `;
-    }else {
-        document.querySelector('#bibl').innerHTML +=`
-        <fieldset border="1px" background-color="red" >
-        <div class="livre">
-            <h2>titre du livre : <br> ${titre.toUpperCase()}</h2>
-            <h2>prix du livre : <br> ${prix}</h2>
-            <div> ${disp} </div>
-        </fieldset>
-        `;
-    }
-}
+    document.querySelector('#bibl').innerHTML +=`
+    <fieldset >
+    <div class="livre">
+        <h2>TITRE DU LIVRE : <br> ${titre.toUpperCase()}</h2>
+        <h2>PRIX DU LIVRE : <br> ${prix}</h2>
+        <div> ${disp} </div>
+    </fieldset>
+    `;
+  
 
 document.forms.fab.addEventListener(
     'submit',
@@ -31,9 +18,8 @@ document.forms.fab.addEventListener(
         createBook(
             this.titre.value,
             this.prix.value,
-            this.disp.value,
+            this.disp.checked,
         );
         return false; 
     }
-); 
-
+) 
